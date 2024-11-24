8a0973404cbe286797d86049fcac91a6b93c83a8
P-15 <dbp-15@proton.me>
2024-11-25 00:09:53 +0100

    Get opened tabs and write urls to clipboard

--- manage-urls/background.js
+++ manage-urls/background.js
@@ -1,7 +1,27 @@
+function onError(error) {
+    console.error(`Error: ${error}`);
+}
+
+function urlsToClipboard(tabs) {
+    navigator.clipboard.writeText("Got inside the function");
+    navigator.clipboard.writeText("The tab list size is " + tabs.length);
+    let urlList = "";
+    for (const tab of tabs) {
+        try {
+            urlList += tab.url + "\n";
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+    navigator.clipboard.writeText(urlList);
+}
+
 function getURLs() {
-    browser.tabs.create({
-        url: "https://developer.mozilla.org",
-    });
+    navigator.clipboard.writeText("This is before getting the tabs");
+    var getTabs = browser.tabs.query({});
+    getTabs.then(urlsToClipboard, onError);
+    navigate.clipboard.writeText("Got back from getting the tabs")
 }
 
 browser.browserAction.onClicked.addListener(getURLs);
+
